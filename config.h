836ad006f45f3012d934f8350467a5ec26cbcd4d
IRoN <pety3bi@gmail.com>
2015-04-28 13:35:17 +0300

    Fixes

--- config.h
+++ config.h
@@ -1,5 +1,5 @@
 /* config.h.  Generated from config.h.in by configure.  */
-/* config.h.in.  Generated from configure.in by autoheader.  */
+/* config.h.in.  Generated from configure.ac by autoheader.  */
 
 /* Define to 1 if you have the <arpa/inet.h> header file. */
 #define HAVE_ARPA_INET_H 1
